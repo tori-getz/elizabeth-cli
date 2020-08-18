0e6c65f0ea40c5a59ff6a59dba58c0f3fc155601
tori-getz <crotecnhics@gmail.com>
2020-08-18 17:03:30 +0300

    Fix background build error

--- src/template/project.template.js
+++ src/template/project.template.js
@@ -18,6 +18,7 @@ module.exports = {
     scenes: [
         {
             name: "MainScene",
+            background: "#000000",
             joyhandler: "JoyHandler",
             entities: []
         }
